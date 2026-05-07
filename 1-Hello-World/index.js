0a3c6c71f6fa4ea0c637b1c8c1a64476932b5f87
JackSoon25 <199736140+JackSoon25@users.noreply.github.com>
2026-05-07 06:28:28 +0000

    complete Hello-word

--- 1-Hello-World/index.js
+++ 1-Hello-World/index.js
@@ -1,1 +1,11 @@
+const url = "https://4geeksacademy.github.io/exercise-assets/txt/hello.txt";
 
+document.querySelector("#showBtn").addEventListener("click", function(){
+    loadData();
+})
+
+async function loadData() {
+    const response = await axios.get(url);
+    console.log(response);
+    document.querySelector("#showContent").innerText = response.data;
+}
